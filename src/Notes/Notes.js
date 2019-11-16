5ca58b757286657a2d4f587440da76d2709eb589
MegaKreja <kreja95ue@gmail.com>
2019-11-16 20:14:04 +0100

    added logic for adding note, displaying all notes and modal

--- src/Notes/Notes.js
+++ src/Notes/Notes.js
@@ -1,5 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import './Notes.css';
+import { Button, Modal } from 'semantic-ui-react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faStickyNote,
+  faCheckCircle,
+  faMinusCircle
+} from '@fortawesome/free-solid-svg-icons';
+import Note from '../Note/Note';
+import NoteForm from '../NoteForm/NoteForm';
+import './Notes.scss';
 import axios from 'axios';
 
 const Notes = props => {
@@ -13,14 +22,86 @@ const Notes = props => {
     axios
       .get('./notes.json')
       .then(notes => {
-        console.log(notes.data);
+        setNotes(notes.data);
       })
       .catch(err => console.log(err));
-  });
+  }, []);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const onChangeTitle = e => {
+    setTitle(e.target.value);
+  };
+
+  const onChangeAuthor = e => {
+    setAuthor(e.target.value);
+  };
+
+  const onChangeBody = e => {
+    setBody(e.target.value);
+  };
+
+  const handleAddNote = () => {
+    const newNotes = [...notes];
+    newNotes.push({ title, body, author });
+    setNotes(newNotes);
+    handleClose();
+  };
+
+  const notesList = notes.map((note, i) => (
+    <Note key={i} title={note.title} body={note.body} author={note.author} />
+  ));
+
+  const addNoteModal = (
+    <Button onClick={handleOpen} className='addNoteBtn' color='blue'>
+      <FontAwesomeIcon className='addNoteIcon' icon={faStickyNote} size='1x' />
+      Add a note
+    </Button>
+  );
 
   return (
     <div className='notes'>
-      <h1>Hello</h1>
+      <h1>Notes</h1>
+      {notesList}
+      <Modal
+        size='tiny'
+        open={open}
+        trigger={addNoteModal}
+        onClose={handleClose}
+      >
+        <Modal.Content>
+          <NoteForm
+            close={handleClose}
+            changeTitle={onChangeTitle}
+            changeAuthor={onChangeAuthor}
+            changeBody={onChangeBody}
+          />
+        </Modal.Content>
+        <Modal.Actions>
+          <Button onClick={handleClose} color='red'>
+            <FontAwesomeIcon
+              className='addNoteIcon'
+              icon={faMinusCircle}
+              size='1x'
+            />
+            No
+          </Button>
+          <Button onClick={handleAddNote} color='green'>
+            <FontAwesomeIcon
+              className='addNoteIcon'
+              icon={faCheckCircle}
+              size='1x'
+            />
+            Yes
+          </Button>
+        </Modal.Actions>
+      </Modal>
     </div>
   );
 };
