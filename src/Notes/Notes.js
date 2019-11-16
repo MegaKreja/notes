b2341e81ce4d568d59ff80b3d03ccf0f237576ae
MegaKreja <kreja95ue@gmail.com>
2019-11-16 13:55:35 +0100

    added state to notes component

--- src/Notes/Notes.js
+++ src/Notes/Notes.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Notes.css';
+import axios from 'axios';
 
-function Notes() {
+const Notes = props => {
+  const [notes, setNotes] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('');
+
+  useEffect(() => {
+    axios
+      .get('./notes.json')
+      .then(notes => {
+        console.log(notes.data);
+      })
+      .catch(err => console.log(err));
+  });
+
   return (
-    <div className="notes">
+    <div className='notes'>
       <h1>Hello</h1>
     </div>
   );
-}
+};
 
 export default Notes;
