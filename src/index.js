2a69624c22e38ad1870a6a152e8805299b25f5fe
MegaKreja <kreja95ue@gmail.com>
2019-11-18 14:43:44 +0100

    deleted react router dom package with drafts component

--- src/index.js
+++ src/index.js
@@ -1,19 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Notes from './components/Notes/Notes';
-import Drafts from './components/Drafts/Drafts';
 import './index.css';
+import App from './Notes/Notes';
 
-const routes = (
-  <BrowserRouter>
-    <div>
-      <Switch>
-        <Route exact={true} path='/' component={Notes} />
-        <Route path='/drafts' component={Drafts} />
-      </Switch>
-    </div>
-  </BrowserRouter>
-);
-
-ReactDOM.render(routes, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
