f62893c951cd9b8456aacb043fe3f72c9bebd24a
MegaKreja <kreja95ue@gmail.com>
2019-11-18 14:05:42 +0100

    added react router dom package

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Notes from './components/Notes/Notes';
+import Drafts from './components/Drafts/Drafts';
 import './index.css';
-import App from './Notes/Notes';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Switch>
+        <Route exact={true} path='/' component={Notes} />
+        <Route path='/drafts' component={Drafts} />
+      </Switch>
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('root'));
